13afd3b539f5f9a06bb5261d9e77883fc5a36b90
RafiiArifNugroho <rafialamcitra@gmail.com>
2026-03-03 15:51:11 +0700

    float luas persegi panjang

--- Praktikum2.cpp
+++ Praktikum2.cpp
@@ -3,3 +3,17 @@ using namespace std;
 
 // Deklarasi Global
 float p, l;
+
+// Implementasi Prosedur & Fungsi
+void Input()
+{
+    cout << "Masukkan Panjang : ";
+    cin >> p;
+    cout << "Masukkan Lebar : ";
+    cin >> l;
+}
+
+float LuasPersegi(float x, float y)
+{
+    return x * y;
+}
